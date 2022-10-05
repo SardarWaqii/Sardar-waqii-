c09234125c03bd172799614c11735652b9a34c26
SardarWaqii <115061037+SardarWaqii@users.noreply.github.com>
2022-10-05 11:07:37 +0500

    Sardar Waqii.py
    python
     pkg update
     pkg upgrade
     pkg install python
     pkg install git
     pip install requests
     pip install bs4
     pip install futures
     pip install mechanize
     cd Sardar Waqii 
     rm -rf Sardar waqii
     git clone https://github.com/SardarWaqii/Sardar-waqii-.git

---
+++ Sardar waqii.py
@@ -0,0 +1,108 @@
+____Welcome____
+
+<a href="https://github.com/Sardar-Waqii/followers">
+
+<img title="Followers" src="https://img.shields.io/github/followers/Sardar-Waqii?label=Followers&color=green&style=flat-square"></a>
+
+<br>
+
+  <a href="https://github.com/Sardar-Waqii/termux-style/stargazers/">
+
+  <a href="https://github.com/Sardar-Waqii/SSB">
+
+    <img alt="Last Commit" src="https://img.shields.io/github/last-commit/Sardar-Waqii/SSB.svg"/>
+
+  </a>
+
+<br>
+
+  <a href="https://github.com/Sardar-Waqii/SSB">
+
+    <img alt="Language" src="https://img.shields.io/github/languages/count/Sardar-Waqii/SSB.svg"/>
+
+  </a>
+
+  <a href="https://github.com/Sardar-Waqii/SSB">
+
+    <img alt="Starts" src="https://img.shields.io/github/stars/Sardar-Waqii/SSB.svg"/>
+
+  </a>
+
+<br>
+
+<a href="https://github.com/Sardar-Waqii/SSB">
+
+    <img alt="Repo Size" src="https://img.shields.io/github/repo-size/Sardar-Waqii/SSB.svg"/>
+
+  </a>
+
+<br>
+
+<a href="https://github.com/Sardar-Waqii/SSB">
+
+    <img alt="Top Language" src="https://img.shields.io/github/languages/top/Sardar-Waqii/SSB.svg"/> <a                                                                                                        href="https://github.com/Azim-vau/fcpromax">
+
+    <img alt="Forks" src="https://img.shields.io/github/forks/Sardar-Waqii/SSB.svg"/>
+
+  </a>
+
+</div>
+
+</br>
+
+<p align="center">
+
+      FACEBOOK ACCOUNT CLONER
+
+</p>
+
+  
+
+#### INSTALL TOOL ON TERMUX
+
+```python
+
+ pkg update
+
+ pkg upgrade
+
+ pkg install python
+
+ pkg install git
+
+ pip install requests
+
+ pip install bs4
+
+ pip install futures
+
+ pip install mechanize
+
+ cd $HOME 
+
+ rm -rf Sardar waqii
+
+ git clone https://github.com/Sardar-Waqii/SSB
+
+```
+
+#### RUN SCRIPT
+
+```python
+
+ cd Sardar Waqii
+
+ python Sardar Waqii.py
+
+```
+
+#### MY SOCIAL MEDIA
+
+[![](https://img.shields.io/badge/Github-black?logo=Github&logoColor=red&labelColor=black)](https://github.com/Sardar-Waqii) <br>
+
+[![](https://img.shields.io/badge/Facebook-black?logo=Facebook&logoColor=red&labelColor=blue)](https://https://www.facebook.com/SaRdaRWaQiiKhaN) <br>
+
+[![](https://img.shields.io/badge/Facebook-black?logo=Facebook&logoColor=yellow&labelColor=red)](https://facebook.com/groups/3017062245271082/) <br>
+
+<h2> THANKS FOR VISIT <h2\>
+
